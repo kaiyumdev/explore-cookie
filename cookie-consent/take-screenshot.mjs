d7c3cd9bb10c9124945fc8b70980c3f66e9e1058
MdKaioumislam5 <afrankaiyum34@gmail.com>
2023-09-25 19:04:09 +0600

    addStyle at cookie

--- cookie-consent/take-screenshot.mjs
+++ cookie-consent/take-screenshot.mjs
@@ -7,12 +7,25 @@ await page.goto("https://www.onetrust.com/products/cookie-consent/", {
   waitUntil: "networkidle2",
 });
 
-await page
-  .waitForSelector("#onetrust-accept-btn-handler", { timeout: 2000 })
-  .catch(() => {
-    console.log("cookie popup not found for 2 seconds");
-  });
-const acceptBtn = await page.$("#onetrust-accept-btn-handler");
-if (acceptBtn) {
-  await acceptBtn.click();
-}
+//wait for selector and then click
+// const acceptBtn = await page
+//   .waitForSelector("#onetrust-accept-btn-handler", { timeout: 2000 })
+//   .catch(() => {
+//     console.log("cookie popup not found for 2 seconds");
+//   });
+// if (acceptBtn) {
+//   await acceptBtn.click();
+// }
+
+//find element and then click
+// const acceptBtn = await page.$("#onetrust-accept-btn-handler");
+// if (acceptBtn) {
+//   await acceptBtn.click();
+// }
+
+//remove cookie consent with evaluate
+// await page.evaluate(() => {
+//   document.querySelector("#onetrust-consent-sdk")?.removeEventListener();
+// });
+
+await page.addStyleTag({ content: `#onetrust-consent-sdk {display: none}` });
